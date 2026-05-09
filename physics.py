bb1a13e390582bad9ba198a291f30f7165ba7e29
Elijah <elsilguero2005@gmail.com>
2026-05-09 12:21:29 -0500

    Make turning friction more realistic - car can slide if going too fast

--- physics.py
+++ physics.py
@@ -1,5 +1,6 @@
 import pygame  
-import  math                          
+import math    
+import sim                      
 
 TIRE_FRIC_COEFF = 1.7
 MECH_FRIC = 50 # N
@@ -11,6 +12,7 @@ TIRE_RAD = 0.36 # m
 GRAVITY = 9.8 # m/s^2
 WHEELBASE = 1.6 # m
 STEER_ANGLE_MAX = 30 # deg
+WALL_ELASTICITY_COEF = 0.7
 dt = 1/60
 
 def Car_Physics(Car):
@@ -25,10 +27,6 @@ def Car_Physics(Car):
 
     vel_normalized = Car.vel.normalize() if vel_mag > 0 else Car.vel
 
-    # Determine how sideways we are for frictional force
-    dir_perp = Car.dir.rotate(90)
-    slide = dir_perp.dot(Car.vel)
-
     # Forces acting on car:
     # Engine torque / tire radius in direction of car
     f_eng = Car.throttle * (ENGINE_TORQUE / TIRE_RAD) * Car.dir
@@ -42,8 +40,12 @@ def Car_Physics(Car):
         f_mech = -vel_normalized*MECH_FRIC
 
     # Friction, opposing the car's sliding
-    f_n = GRAVITY * CAR_MASS + (DRAG_COEFF/4 * vel_mag**2) # gravity + downforce
-    f_fric = TIRE_FRIC_COEFF * f_n * -dir_perp * slide
+    dir_perp = Car.dir.rotate(90) # Vector for the car's side direction
+    vel_lat = Car.vel.project(dir_perp) # How much of our velocity vector is lateral to the car
+    force_needed = (CAR_MASS*vel_lat/dt).magnitude() # The force that will stop our sliding
+    f_n = GRAVITY * CAR_MASS + (DRAG_COEFF * vel_mag**2) # gravity + downforce
+    f_fric_max = TIRE_FRIC_COEFF * f_n
+    f_fric = min(f_fric_max, force_needed)*-vel_lat.normalize() if vel_lat.magnitude() > 0 else pygame.math.Vector2(0,0) # friction will oppose sliding until it can't
 
     # Net force
     f_net = f_eng + f_drag + f_mech + f_fric
@@ -72,3 +74,9 @@ def Car_Physics(Car):
 
     Car.vel.update(new_vel)
     Car.dir.update(new_dir)
+
+def Car_Collisions(Car, map):
+    # Check for car colliding with wall
+    wall_hit_list = pygame.sprite.spritecollide(Car, map, False)
+    for wall in wall_hit_list:
+        Car.vel.update((Car.vel.reflect(wall.normal)*WALL_ELASTICITY_COEF))
